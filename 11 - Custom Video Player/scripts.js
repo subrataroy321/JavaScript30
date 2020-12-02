6e1a84ed3f1418428c15f0dc5a292a66f8e658ac
SUBRATA ROY <subrata.r321@gmail.com>
2020-12-01 20:56:17 -0500

    finished 11 exercise

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,7 +3,12 @@
 const player = document.querySelector('.player')
 const video = document.querySelector('.viewer')
 const playButton = document.querySelector('.toggle')
-const skipButtons = player.querySelectorAll('[data-skip]')
+const skipButtons = document.querySelectorAll('[data-skip]')
+const playbackSlider = document.querySelector('[name="playbackRate"]')
+const sliders = document.querySelectorAll('.player__slider')
+const progress = document.querySelector('.progress__filled')
+const progressBar = document.querySelector('.progress')
+const fullscreenButton = document.querySelector('.fullscreen__button')
 
 const togglePlay = () => {
     if (video.paused) {
@@ -26,9 +31,30 @@ video.addEventListener('pause', updateButton);
 playButton.addEventListener('click', togglePlay);
 
 const skipVideo = (time) => {
-    video.currentTime += parseInt(time.skip) 
+    video.currentTime += parseFloat(time.skip) 
 }
 
 skipButtons.forEach(button => {
     button.addEventListener('click', () => skipVideo(button.dataset))
+})
+
+sliders.forEach(slider => {
+    slider.addEventListener('change', () => {
+        video[slider.name] = slider.value;
+    } )
+})
+
+video.addEventListener('timeupdate', () => {
+    const percent = (video.currentTime / video.duration) * 100 ;
+    progress.style.flexBasis = `${percent}%`
+})
+
+progressBar.addEventListener('click', (e) => {
+    const scrubTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+})
+
+fullscreenButton.addEventListener('click', () => {
+    video.requestFullscreen()
+    console.log(requestFullscreen)
 })
